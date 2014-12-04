60d2f6fef4db83925a935886addb33a89de9568e
Hooman Peiro Sajjad <shps@kth.se>
2014-12-04 14:39:07 +0100

    Improved output. Implemented a method to run all different combination of applying gc and mgroup to get a better comparison .# Please enter the commit message for your changes. Lines starting

--- src/se/kth/mcac/CommunityFinder.java
+++ src/se/kth/mcac/CommunityFinder.java
@@ -8,7 +8,6 @@ import se.kth.mcac.graph.Graph;
 import se.kth.mcac.util.CsvConvertor;
 import se.kth.mcac.util.ModularityComputer;
 import se.kth.mcac.util.SpaceSeparatedConvertor;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 /**
  *
@@ -20,7 +19,7 @@ public class CommunityFinder {
     static final String DEFAULT_FILE_DIR = String.format("/home/hooman/Desktop/dimacs/%s/", GRAPH_NAME);
     static final String FILE_NAME = GRAPH_NAME + ".graph";
     static final int START_ITERATION = 1;
-    static final int END_ITERATION = START_ITERATION + 50;
+    static final int END_ITERATION = START_ITERATION + 10;
     static final int INCREMENT_PER_ITERATION = 1;
     static final boolean APPLY_MGROUP = true;
     static final int APPLY_MGROUP_AFTER = 0;
@@ -37,28 +36,82 @@ public class CommunityFinder {
 
     public static void main(String[] args) throws IOException, Exception {
 
-        boolean mgroup = APPLY_MGROUP;
-        boolean gc = USE_GARBAGE_COLLECTOR;
+        if (FULL_STATS) {
+            executeAll();
+        } else {
 
-        HashMap<StatKeys, Object> stats = execute(mgroup, gc);
+            boolean mgroup = APPLY_MGROUP;
+            boolean gc = USE_GARBAGE_COLLECTOR;
 
-        int maxRound = (int) stats.get(StatKeys.ITERATION);
-        double maxModularity = (double) stats.get(StatKeys.MAX_MODULARITY);
-        double beforeMgroupModularity = (double) stats.get(StatKeys.BEFORE_MGROUP_MOD);
-        boolean appliedMGroup = (boolean) stats.get(StatKeys.APPLIED_MGROUP);
-        int maxNumCom = (int) stats.get(StatKeys.NUM_COM);
-        int beforeMgroupNumCom = (int) stats.get(StatKeys.BEFORE_MGROUP_NUM_COM);
+            HashMap<StatKeys, Object> stats = execute(mgroup, gc, "");
 
-        print(String.format("Max Modularity = %f, Number of Communities = %d, Iteration = %d, "
-                + "AppliedMgroup = %b, BeforeMgroupModularity = %f, BeforeMgroupNumCom = %d",
-                maxModularity, maxNumCom, maxRound, appliedMGroup, beforeMgroupModularity, beforeMgroupNumCom));
+            print(String.format("**** Results ****\nMax Modularity = %f\nNumber of Communities = %d\nIteration = %d\n "
+                    + "AppliedMgroup = %b\nBeforeMgroupModularity = %f\nBeforeMgroupNumCom = %d",
+                    (double) stats.get(StatKeys.MAX_MODULARITY),
+                    (int) stats.get(StatKeys.NUM_COM),
+                    (int) stats.get(StatKeys.ITERATION),
+                    (boolean) stats.get(StatKeys.APPLIED_MGROUP),
+                    (double) stats.get(StatKeys.BEFORE_MGROUP_MOD),
+                    (int) stats.get(StatKeys.BEFORE_MGROUP_NUM_COM)));
+        }
     }
 
-    private static HashMap<StatKeys, Object> executeAll() throws Exception {
-        return null;
+    private static void executeAll() throws Exception {
+
+        // No MGroup, No GC
+        boolean mgroup = false;
+        boolean gc = false;
+        HashMap<StatKeys, Object> stats1 = execute(mgroup, gc, "nmng");
+
+        // Apply Mgroup, No GC
+        mgroup = true;
+        gc = false;
+        HashMap<StatKeys, Object> stats2 = execute(mgroup, gc, "mng");
+
+        // No Mgroup, Apply GC
+        mgroup = false;
+        gc = true;
+        HashMap<StatKeys, Object> stats3 = execute(mgroup, gc, "nmg");
+
+        // Apply Mgroup, Apply GC
+        mgroup = true;
+        gc = true;
+        HashMap<StatKeys, Object> stats4 = execute(mgroup, gc, "mg");
+
+        print(String.format("**** NO MGroup, No GC ****\nMax Modularity = %f\nNumber of Communities = %d\nIteration = %d\n"
+                + "AppliedMgroup = %b",
+                (double) stats1.get(StatKeys.MAX_MODULARITY),
+                (int) stats1.get(StatKeys.NUM_COM),
+                (int) stats1.get(StatKeys.ITERATION),
+                (boolean) stats1.get(StatKeys.APPLIED_MGROUP)));
+
+        print(String.format("**** Applied MGroup, No GC ****\nMax Modularity = %f\nNumber of Communities = %d\nIteration = %d\n"
+                + "AppliedMgroup = %b\nBeforeMgroupModularity = %f\nBeforeMgroupNumCom = %d",
+                (double) stats2.get(StatKeys.MAX_MODULARITY),
+                (int) stats2.get(StatKeys.NUM_COM),
+                (int) stats2.get(StatKeys.ITERATION),
+                (boolean) stats2.get(StatKeys.APPLIED_MGROUP),
+                (double) stats2.get(StatKeys.BEFORE_MGROUP_MOD),
+                (int) stats2.get(StatKeys.BEFORE_MGROUP_NUM_COM)));
+
+        print(String.format("**** NO MGroup, Applied GC ****\nMax Modularity = %f\nNumber of Communities = %d\nIteration = %d\n"
+                + "AppliedMgroup = %b",
+                (double) stats3.get(StatKeys.MAX_MODULARITY),
+                (int) stats3.get(StatKeys.NUM_COM),
+                (int) stats3.get(StatKeys.ITERATION),
+                (boolean) stats3.get(StatKeys.APPLIED_MGROUP)));
+
+        print(String.format("**** Applied MGroup, Applied GC ****\nMax Modularity = %f\nNumber of Communities = %d\nIteration = %d\n"
+                + "AppliedMgroup = %b\nBeforeMgroupModularity = %f\nBeforeMgroupNumCom = %d",
+                (double) stats4.get(StatKeys.MAX_MODULARITY),
+                (int) stats4.get(StatKeys.NUM_COM),
+                (int) stats4.get(StatKeys.ITERATION),
+                (boolean) stats4.get(StatKeys.APPLIED_MGROUP),
+                (double) stats4.get(StatKeys.BEFORE_MGROUP_MOD),
+                (int) stats4.get(StatKeys.BEFORE_MGROUP_NUM_COM)));
     }
 
-    private static HashMap<StatKeys, Object> execute(boolean mgroup, boolean gc) throws Exception {
+    private static HashMap<StatKeys, Object> execute(boolean mgroup, boolean gc, String filePrefix) throws Exception {
         SpaceSeparatedConvertor convertor = new SpaceSeparatedConvertor();
         Graph g = convertor.convertToGraph(DEFAULT_FILE_DIR + FILE_NAME);
         print(String.format("Graph Nodes = %d, Edges = %d", g.size(), g.getNumOfEdges() / 2));
@@ -86,7 +139,7 @@ public class CommunityFinder {
             after = System.currentTimeMillis();
             print(String.format("Modularity = %f", modularity));
             print(String.format("Computation time for modularity is %d", after - before));
-            CsvConvertor.convertAndWrite(g, String.format("%s%d", DEFAULT_FILE_DIR, round));
+            CsvConvertor.convertAndWrite(g, String.format("%s%s%d", DEFAULT_FILE_DIR, filePrefix, round));
 
             if (modularity > maxModularity) {
                 maxRound = round;
@@ -102,13 +155,13 @@ public class CommunityFinder {
                 before = System.currentTimeMillis();
                 cd.findCommunities(g);
                 after = System.currentTimeMillis();
-                System.out.println(String.format("Computation time for MGroup is %d", after - before));
+                print(String.format("Computation time for MGroup is %d", after - before));
                 double temp = modularity;
                 modularity = ModularityComputer.compute(g);
                 print(String.format("After MGroup Modularity = %f", modularity));
                 int newNumCom = g.getNumCommunities();
                 print(String.format("Number of Communities %d", newNumCom));
-                CsvConvertor.convertAndWrite(g, String.format("%s%dmgroup", DEFAULT_FILE_DIR, round));
+                CsvConvertor.convertAndWrite(g, String.format("%s%s%dmgroup", DEFAULT_FILE_DIR, filePrefix, round));
 
                 if (modularity > maxModularity) {
                     maxRound = round;
